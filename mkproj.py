132b159c26166135f97e110e0fb984a8c40f9c78
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-07-07 12:32:34 -0300

    updates

--- mkproj.py
+++ mkproj.py
@@ -1,3 +1,5 @@
 # script for creating venvs several languages (python, java, C++ (?), etc). For python,
 # there'll be the possibility of pre-installing certain packages (numpy, pandas, etc)
 
+import subprocess
+import argparse
