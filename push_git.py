ed0d3712f23a6010844e56f610cebddf8b8d5845
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-07-08 19:43:18 -0300

    [debugged push_git]

--- push_git.py
+++ push_git.py
@@ -5,10 +5,11 @@ def main():
     parser.add_argument('args', nargs=1, type=str)
     parser.add_argument('-b' ,'--branch', default=settings.default_branch)
     args = parser.parse_args()
-
-    subprocess.run('git add .', cwd='.', shell=True)
-    subprocess.run(f'git commit -m {args.args}', cwd='.', shell=True)
-    subprocess.run(f'git push -u origin {args.branch}', cwd='.', shell=True)
+    subprocess.run(f'git add .', cwd='.', shell=True, errors='raise')
+    subprocess.run(f'git commit -m {args.args}',
+                   cwd='.', shell=True, errors='raise')
+    subprocess.run(
+        f'git push -u origin {args.branch}', cwd='.', shell=True, errors='raise')
 
 if __name__ == '__main__':
     main()
