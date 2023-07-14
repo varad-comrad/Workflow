15833afdfb641f730cf6ce8400de9fb644c60a43
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-07-14 16:08:31 -0300

    TODO: finish implementation of mkdb.py

---
+++ dbsettings.py
@@ -0,0 +1,60 @@
+db_session = '''
+import sqlalchemy as sa
+from sqlalchemy.future.engine import Engine
+from sqlalchemy.orm import sessionmaker, Session
+import pathlib
+from models.model_base import ModelBase
+
+__engine: Engine | None = None
+
+def create_engine(sqlite: bool=False, postgresql: bool=False) -> Engine:
+    global __engine
+    if __engine:
+        return __engine
+    if sqlite:
+        path = pathlib.Path('{}/{}.sqlite') #! variable here
+        path.parent.mkdir(parents=True, exist_ok=True) 
+        conn_str = f'sqlite:///' + path
+        __engine = sa.create_engine(url=conn_str, echo=False)
+    elif postgresql:
+        conn_str = '{}://{}:{}@localhost:{}/{}'  # ! variable here
+        __engine = sa.create_engine(url=conn_str, echo=False)
+    return __engine
+
+def create_session() -> Session:
+    global __engine
+    if not __engine:
+        create_engine() #! variable here
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+    session: Session = __session()
+    return session
+
+
+def create_tables() -> None:
+    global __engine
+    if not __engine:
+        create_engine() #! variable here
+    import models.__all_models
+    ModelBase.metadata.drop_all(__engine)
+    ModelBase.metadata.create_all(__engine)
+
+'''
+
+model_base = '''
+import sqlalchemy.ext.declarative as decl
+
+ModelBase = decl.declarative_base()
+'''
+
+create_main='''
+from conf.db_session import create_tables
+
+
+if __name__ == '__main__':
+    create_tables()
+'''
+
+databases_ports: dict = {
+    'postgres': 5432,
+    'sqlite': None
+}
--- mkdb.py
+++ mkdb.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import argparse
-import pathlib
+import argparse, pathlib, dbsettings, subprocess
 
 
 def db_parser():
@@ -13,7 +12,9 @@ def db_parser():
     parser.add_argument('args', type=str, nargs=1)
     parser.add_argument('-db', '--database', required=True, type=str, choices=databases)
     parser.add_argument('-d', '--dir', default='.', type=str)
-
+    parser.add_argument('-U', '--user', type=str)
+    parser.add_argument('-pwd', '--password', type=str)
+    parser.add_argument('-n', '--database-name', required=True, type=str)
 
     return parser
 
@@ -24,35 +25,30 @@ class DatabaseCreator:
         self._dir = self._dir.joinpath(f'{parsed_args.args[0]}')
         self._dir.mkdir(parents=True)
 
-    def create_sqlite(self):
+    def create_database(self):
         conf = self._dir.joinpath('conf') 
         conf.mkdir()
         models = self._dir.joinpath('models')
         models.mkdir()
 
-        self._dir.joinpath('requirements.txt').touch()
-        self._dir.joinpath('create_main.py').touch()
-        conf.joinpath('db_session.py').touch()
+        with self._dir.joinpath('requirements.txt').open('w') as requirements_file:
+            pass
+        with self._dir.joinpath('create_main.py').open('w') as create_main_file:
+            create_main_file.write(dbsettings.create_main)
+        with conf.joinpath('db_session.py').open('w') as db_session_file:
+            db_session_file.write(dbsettings.db_session.format('something',
+                                                                'other stuff',
+                                                                self._db, 
+                                                                'user', 
+                                                                'password', 
+                                                                dbsettings.databases_ports[self._db], 
+                                                                'namedb'))
         models.joinpath('__all_models.py').touch()
-        models.joinpath('model_base.py').touch()
+        with models.joinpath('model_base.py').open('w') as model_base_file:
+            model_base_file.write(dbsettings.model_base)
 
-        def create_mongodb(self):
-            pass
-
-        def create_postgre(self):
-            pass
-
-        def create_mysql(self):
-            pass
-
-        def create_microsoft_sql(self):
-            pass
-
-        def create_surreal(self):
-            pass
-        
 
 if __name__ == '__main__':
-    DatabaseCreator(db_parser().parse_args()).create_sqlite()
+    DatabaseCreator(db_parser().parse_args()).create_database()
 
         
