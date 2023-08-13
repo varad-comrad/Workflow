19b734d8fa020cc9622bad746a80a42a498b5aef
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-13 12:58:31 -0300

    debug needed in setup.py: unknown problem related to file permissions

--- workflow/setup.py
+++ workflow/setup.py
@@ -1,31 +1,162 @@
-import json, pathlib, subprocess
+import json
+import pathlib
+import subprocess
+import shutil
 
 
-# ask for user input here
-def initial_configs():
-	with open('settings.json', 'w') as f:
-		file = {"default_dir": "", "default_branch": "master", "venv_manager": "pyenv", "db_username": "",
-				"db_password": 0, "path_to_sqlite": "db/data.db", "default_db_host": "localhost", "poetry_extension": "--src", "conda_extension": "-y", "pyenv_extension": ""}
-		json.dump(file, f)
+def create_workflow_directory() -> pathlib.Path:
+	home = pathlib.Path(
+		f'/home/{subprocess.run("whoami", shell=True,  capture_output=True, text=True).stdout.strip()}')
+	name = '.workflow'
+	while True:
+		try:
+			(home / name).mkdir(exist_ok=False)
+			break
+		except:
+			print(f"Could not setup workflow, a folder with name {name} already exists")
+			name = input("Write the name you wish for it: ")
 
-def create_workflow_directory():
-	home = pathlib.Path(f'/home/{subprocess.run("whoami", shell=True)}')
-	try:
-		(home / '.workflow').mkdir(exist_ok=False)
-	except:
-		print("Could not setup workflow, a folder with name .workflow already exists")
 	rcs = []
 	for element in home.iterdir():
-		if element == '.zshrc':
+		if element.name == '.zshrc':
 			rcs.append(home / '.zshrc')
-		if element == '.fishrc':
+		if element.name == '.fishrc':
 			rcs.append(home / '.fishrc')
-		if element == '.bashrc':
+		if element.name == '.bashrc':
 			rcs.append(home / '.bashrc')
-		if element == '.zshrc':
-			rcs.append(home / '.zshrc')
+		# if element == '.zshrc':
+		# 	rcs.append(home / '.zshrc')
 
 	for rc in rcs:
 		with rc.open('a') as file:
-			rc.write(f'\n\nPATH =$PATH: ~/.workflow/workflow\nsource ~/.workflow/workflow/scripts.zsh\n\n')
+			file.write(
+				f'\n\nhome_dir_workflow=~/{name}\nPATH=$PATH: $home_dir_workflow\nsource $home_dir_workflow/scripts.zsh\n\n')
+	return home/name
 
+
+def move_directory(directory: pathlib.Path, destination: pathlib.Path):
+	shutil.copytree(directory, destination, dirs_exist_ok=True)
+
+# ask for user input here
+def initial_configs(dir: pathlib.Path):
+	...
+	with (dir/'settings.json').open('w') as f:
+		file = {"default_dir": "", "default_branch": "master", "venv_manager": "pyenv", "db_username": "",
+			"db_password": 0, "path_to_sqlite": "db/data.db", "default_db_host": "localhost", "poetry_extension": "--src", "conda_extension": "-y", "pyenv_extension": ""}
+		json.dump(file, f)
+
+
+def chmod(dir: pathlib.Path, mode: int = 777):
+	subprocess.run(f'sudo chmod {mode} {dir.absolute()}', shell=True)
+	# dir.chmod(mode)
+	for element in dir.iterdir():
+		if element.is_dir():
+			chmod(element, mode)
+		element.chmod(mode)
+
+
+def create_scripts_sh(dir: pathlib.Path):
+	template = '''
+function resetter(){
+    if [ $1 = 'commit' ]; then
+        shift
+        reset_commit $@
+
+    fi 
+}
+
+function reset_commit() {
+  local commit_number=${1:-"1"}
+  local commit_reference="HEAD~$commit_number"
+  local commit_hash=$(git rev-parse "$commit_reference")
+  git reset --hard "$commit_hash"
+}
+
+function workon(){
+	local p=$1
+    # TODO: create venv if doesn't exist
+	source $1/bin/activate
+}
+
+function pythonproj(){
+	msg=$(pyproj.py $@)
+    vman=$(echo $msg | tail -n 1)
+    eval $vman
+}
+
+function mkproj(){
+	mkproj.py $@
+}
+
+function javaproj(){
+	javaproj.py $@
+}
+
+function mkdb(){
+    mkdb.py $@
+}
+
+function push_git(){
+	push_git.py $@
+}
+
+function mkdirproj(){
+	mkdirproj.py $@
+}
+
+function activate_shell(){
+    shell.py 
+}
+
+function new_function(){
+    make_workflow.py $@
+}
+
+function workflow(){
+
+    if [ $# -eq 0 ]; then
+        activate_shell
+    elif [ $1 = 'config' ]; then
+        shift
+        config.py "$@" 
+    elif [ $1 = 'mkdir' ]; then
+        shift
+        mkdirproj "$@" 
+    elif [ $1 = 'pythonproj' ]; then
+        shift
+        pythonproj "$@" 
+    elif  [ $1 = 'mkdb' ]; then
+        shift
+        mkdb "$@" 
+    elif  [ $1 = 'push' ]; then
+        shift
+        push_git "$@" 
+    elif [ $1 = 'new' ]; then
+        shift
+        new_function "$@"
+    elif [ $1 = 'reset' ]; then
+        shift
+        resetter "$@"
+    elif [ $1 = '-h' ]; then
+        cat "$home_dir_workflow/text_files/advanced_helper.txt"
+    else
+        cat "$home_dir_workflow/text_files/helper.txt"
+    fi
+
+}
+'''
+	with (dir/ 'scripts.sh').open('w') as file:
+		file.write(template)
+	pass
+
+
+def main():
+	path = create_workflow_directory()
+	move_directory(pathlib.Path('.') / 'workflow', path)
+	initial_configs(path)
+	chmod(path)
+	create_scripts_sh(path)
+
+
+if __name__ == '__main__':
+	main()
--- workflow/shell.py
+++ workflow/shell.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python
 
-import cmd2, subprocess, colorama, settings
+from typing import Dict, Iterable, List, Optional, TextIO
+import cmd2
+import subprocess
+import colorama
+import settings
+from cmd2.command_definition import CommandSet
+
 
 class Shell(cmd2.Cmd):
-    
+
     prompt = f"{colorama.Fore.BLUE}{subprocess.run('whoami', shell=True, capture_output=True, text=True).stdout.strip()}@{colorama.Fore.GREEN}{subprocess.run('hostname', shell=True, capture_output=True, text=True).stdout.strip()}{colorama.Fore.LIGHTBLUE_EX}:{colorama.Fore.BLUE}~{colorama.Style.RESET_ALL}$ "
+
+    # highlighted_keywords = ['exit', 'mkdb', 'config', 'mkdir',
+    #                         'pyproj', 'new', 'push', 'bash', 'clear', 'git']
+
+    # colors = {
+    #     'exit': colorama.Fore.CYAN,
+    #     'mkdb': colorama.Fore.CYAN,
+    #     'config': colorama.Fore.CYAN,
+    #     'mkdir': colorama.Fore.CYAN,
+    #     'pyproj': colorama.Fore.CYAN,
+    #     'new': colorama.Fore.CYAN,
+    #     'push': colorama.Fore.CYAN,
+    #     'bash': colorama.Fore.CYAN,
+    #     'clear': colorama.Fore.CYAN,
+    #     'git': colorama.Fore.CYAN
+    # }
+
+    # def precmd(self, statement):
+    #     # Apply syntax highlighting to the command if it's a keyword
+    #     if statement.command in self.highlighted_keywords:
+    #         statement.command = self.colorize(
+    #             statement.command, color=colorama.Fore.CYAN)
+    #     return statement
 
     def do_run(self, arg):
         if settings.s['venv_manager'] == 'poetry':
-            pass # subprocess.run(['poetry shell', arg], shell=True)
+            pass  # subprocess.run(['poetry shell', arg], shell=True)
         else:
-            pass # subprocess.run(arg, shell=True)
+            pass  # subprocess.run(arg, shell=True)
         pass
 
     def do_config(self, arg):
@@ -33,16 +62,17 @@ class Shell(cmd2.Cmd):
 
     def do_bash(self, arg):
         subprocess.run(arg, shell=True)
-    
+
     def do_clear(self, arg):
         subprocess.run('clear', shell=True)
-    
+
     def do_git(self, arg):
         subprocess.run('git ' + arg, shell=True)
-    
+
     def do_exit(self, arg):
         return True
-    
+
+
 if __name__ == '__main__':
     cli = Shell()
     cli.cmdloop()
