47f7efb0ae108d1581635faea79eff58a385b6ee
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-30 08:35:47 -0300

    improving commit message in push_git

--- workflow/push_git.py
+++ workflow/push_git.py
@@ -2,7 +2,7 @@
 import argparse, subprocess, logging
 
 logging.basicConfig(level=logging.INFO,
-                    format="\033[35m[commit]\x1b[92m %(message)s\x1b[0m")
+                    format="ESC[38;5;20m[commit]\x1b[92m %(message)s\x1b[0m")
 
 
 
