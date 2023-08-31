9c3701e8ede2bee759ea60c6c60d57a4d3fef6cf
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-31 16:57:40 -0300

    experiment with git stash

--- workflow/push_git.py
+++ workflow/push_git.py
@@ -4,30 +4,29 @@ import argparse, subprocess, logging
 logging.basicConfig(level=logging.INFO,
                     format="\x1b[38;5;20m[commit]\x1b[92m %(message)s\x1b[0m")
 
-
+def filtering(line: str):
+    if line.startswith('*'):
+        return line[1:].strip()
+    return line.strip()
 
 def branch_exists(branch: str) -> bool:
     lines = subprocess.run(
-        'git branch -a', cwd='.', shell=True, capture_output=True, text=True).stdout.strip().splitlines()
-    aux = [line.strip() for line in lines]
-    return (('* ' + branch) in aux)
+        'git branch', cwd='.', shell=True, capture_output=True, text=True).stdout.strip().splitlines()
+    aux = [filtering(line) for line in lines]
+    return ('* ' + branch) in aux or branch in aux
     #     return ''
     # else:
     #     return '-b'
 
-def stash(message):
-    subprocess.run(f'git stash save "stashed {message}"', cwd='.', shell=True)
-    pass
-
-def unstash_and_checkout(message):
-    subprocess.run(f'git checkout {message}', cwd='.', shell=True)
-    subprocess.run(f'git stash pop', cwd='.', shell=True)
+def stash_and_checkout(message, branch):
+    subprocess.run(
+        f'git stash save "{message}" && git checkout {branch} && git stash pop', cwd='.', shell=True)
     commit_changes(message)
 
-def git_push(branch, extension=''):
+def git_push(branch):
     subprocess.run(f'git push -u origin {branch}', cwd='.', shell=True)
 
-def git_add(files):
+def git_add(files=[]):
     if len(files) > 0:
         subprocess.run(f'git add {" ".join(files)}', cwd='.', shell=True)
     else:
@@ -37,7 +36,7 @@ def git_commit(message):
     subprocess.run(f"git commit -m '{message}'",cwd='.', shell=True)
 
 def commit_changes(message, files=None):
-    git_add(files or [])
+    git_add(files)
     git_commit(message)
 
 def main():
@@ -51,13 +50,13 @@ def main():
         'git branch', shell=True, capture_output=True, text=True).stdout.split('*')[1].strip().split('\n')[0]
     
     if branch_exists(args.branch):
-        stash(args.args)
-        unstash_and_checkout(args.args)
+        breakpoint()
+        stash_and_checkout(args.args[0], args.branch)
+        commit_changes(args.args[0], args.files)
         git_push(args.branch)
         return
     commit_changes(args.args[0], args.files)
     extension = '-b' if not branch_exists(args.branch) else ''
-    git_push(current_branch, extension)
     if args.branch:
         current_branch = args.branch
         subprocess.run(f'git checkout {extension} {args.branch} && git push -u origin {current_branch}', cwd='.', shell=True)
